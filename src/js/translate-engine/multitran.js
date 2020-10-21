830bfdabee0d19cdd06a3f8e34a8129edfaeab54
Vitaly Yerofeyevsky <flytaly@gmail.com>
2020-10-22 00:14:28 +0300

    Translate in both directions

--- src/js/translate-engine/multitran.js
+++ src/js/translate-engine/multitran.js
@@ -15,12 +15,33 @@ export async function fetchPage(text, langFrom, langTo, interfaceLang) {
     }
 }
 
-export async function multitranData(text, langFrom, langTo, interfaceLang = 1) {
+async function getParsedData(text, langFrom, langTo, interfaceLang = 1) {
     const page = await fetchPage(text, langFrom, langTo, interfaceLang);
     if (page.error) return { error: page.error };
     if (!page || !page.textData) return { data: [] };
 
     const parsed = parser(page.textData);
+    return parsed;
+}
 
+/**
+ * Get translation data.
+ * If there is translation in reverse order (e.g. English-Spanish -> Spanish-English)
+ * make another request.
+ */
+export async function multitranData(text, langFrom, langTo, interfaceLang = 1) {
+    const parsed = await getParsedData(text, langFrom, langTo, interfaceLang);
+    const { otherLang } = parsed;
+    const hasReverseTranslation =
+        otherLang &&
+        otherLang.length &&
+        otherLang.some(({ href }) => {
+            const params = new URL(href).searchParams;
+            return params.get('l1') === langTo && params.get('l2') === langFrom;
+        });
+
+    if (hasReverseTranslation) {
+        return getParsedData(text, langTo, langFrom, interfaceLang);
+    }
     return parsed;
 }
