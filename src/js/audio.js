99e3feb9d95f31c2d65499c1e8e760bdaba3a72a
Vitaly Yerofeyevsky <flytaly@gmail.com>
2020-10-23 01:33:47 +0300

    Fixes for FF

--- src/js/audio.js
+++ src/js/audio.js
@@ -16,14 +16,16 @@ const getFlagUrl = (title) => {
  */
 export const addAudioElements = (container, audioFiles) => {
     const flags = [];
-    if (!container) return;
+    if (!container || !audioFiles) return;
     audioFiles.forEach((file) => {
         if (!file.flag) return;
         const flagUrl = browser.runtime.getURL(file.flag);
         if (flagUrl) {
             const flagButton = document.createElement('button');
             const img = document.createElement('img');
-            const audio = new Audio(file.url);
+            // new Audio(url) is blocked in FF by Content Security Policy
+            const audio = document.createElement('audio');
+            audio.src = file.url;
             flagButton.title = file.title;
             img.src = flagUrl;
             flagButton.appendChild(img);
