91436d2b9abd496d43ce9a8055d0278e4d53ca3a
Vitaly Yerofeyevsky <flytaly@gmail.com>
2022-09-19 01:59:42 +0300

    Show spinner during audio file loading

--- src/js/audio.js
+++ src/js/audio.js
@@ -12,14 +12,30 @@ export const addAudioElements = async (container, audioFiles) => {
         if (!file.flag) return;
         const flagUrl = browser.runtime.getURL(file.flag);
         if (flagUrl) {
+            /** @type {HTMLButtonElement} */
             const flagButton = await getTemplate('flag-button');
             const img = flagButton.querySelector('img');
+            const spinner = flagButton.querySelector('svg');
             flagButton.title = file.title;
             img.src = flagUrl;
             flagButton.appendChild(img);
             flagButton.onclick = () => {
                 // new Audio(url) is blocked in FF by Content Security Policy
                 const audio = document.createElement('audio');
+                audio.onloadstart = () => {
+                    img.style.opacity = '0.4' 
+                    spinner?.classList.remove('hidden');
+                };
+                audio.onloadeddata = () => {
+                    img.style.opacity = '1' 
+                    spinner?.classList.add('hidden');
+                }
+                audio.onplay = () => {
+                    flagButton.disabled = true
+                }
+                audio.onended = () => {
+                    flagButton.disabled = false
+                }
                 audio.src = file.url;
                 audio.play();
             };
