63196f13df9b80d0c18226c1116fde45ebeeac77
Vitaly Yerofeyevsky <flytaly@gmail.com>
2022-09-18 20:20:17 +0300

    Upgrade manifest V2->V3 (Chrome only)

--- src/manifest.js
+++ src/manifest.js
@@ -1,22 +1,6 @@
 import pkg from '../package.json';
 import { TARGET } from '../scripts/utils.js';
 
-function browserSpecific() {
-    const manifest = {};
-    if (TARGET === 'chrome') {
-        manifest.minimum_chrome_version = '80';
-    }
-    if (TARGET === 'firefox') {
-        manifest.applications = {
-            gecko: {
-                strict_min_version: '75.0',
-                id: 'multitran@flytaly',
-            },
-        };
-    }
-    return manifest;
-}
-
 const info = {
     default_locale: 'en',
     description: '__MSG_description__',
@@ -26,35 +10,100 @@ const info = {
     homepage_url: 'https://github.com/flytaly/multitran-extension',
 };
 
-/* Firefox only */
-const themeIcons =
-    TARGET === 'firefox'
-        ? {
-              theme_icons: [
-                  {
-                      dark: 'images/icon-16.png',
-                      light: 'images/icon_light-16.png',
-                      size: 16,
-                  },
-                  {
-                      dark: 'images/icon-32.png',
-                      light: 'images/icon_light-32.png',
-                      size: 32,
-                  },
-                  {
-                      dark: 'images/icon-64.png',
-                      light: 'images/icon_light-64.png',
-                      size: 64,
-                  },
-              ],
-          }
-        : {};
+const action = {
+    default_title: '__MSG_extension_title__',
+    default_icon: {
+        16: 'images/icon_light-16.png',
+        32: 'images/icon_light-32.png',
+        64: 'images/icon_light-64.png',
+    },
+    theme_icons: [
+        {
+            dark: 'images/icon-16.png',
+            light: 'images/icon_light-16.png',
+            size: 16,
+        },
+        {
+            dark: 'images/icon-32.png',
+            light: 'images/icon_light-32.png',
+            size: 32,
+        },
+        {
+            dark: 'images/icon-64.png',
+            light: 'images/icon_light-64.png',
+            size: 64,
+        },
+    ],
+    default_popup: 'popup.html',
+};
+
+function browserSpecific() {
+    const bgScript = './dist/background/background.js';
+    let manifest = {};
+    if (TARGET === 'chrome') {
+        manifest = {
+            minimum_chrome_version: '94',
+            background: {
+                service_worker: bgScript,
+            },
+            permissions: ['storage', 'contextMenus'],
+            host_permissions: [
+                'https://www.multitran.com/*',
+                'https://*.wiktionary.org/api/*',
+                'https://*.wikimedia.org/*',
+            ],
+            web_accessible_resources: [
+                {
+                    resources: ['templates.html', 'dist/styles.css', 'images/flags/*.svg'],
+                    matches: ['<all_urls>'],
+                },
+            ],
+            action,
+            commands: {
+                _execute_action: {
+                    suggested_key: {
+                        default: 'Alt+M',
+                    },
+                },
+            },
+        };
+    }
+    if (TARGET === 'firefox') {
+        manifest = {
+            manifest_version: 2,
+            applications: {
+                gecko: {
+                    strict_min_version: '101',
+                    id: 'multitran@flytaly',
+                },
+            },
+            background: {
+                scripts: [bgScript],
+            },
+            permissions: [
+                'storage',
+                'contextMenus',
+                'https://www.multitran.com/*',
+                'https://*.wiktionary.org/api/*',
+                'https://*.wikimedia.org/*',
+            ],
+            browser_action: action,
+            web_accessible_resources: ['templates.html', 'dist/styles.css', 'images/flags/*.svg'],
+            commands: {
+                _execute_browser_action: {
+                    suggested_key: {
+                        default: 'Alt+M',
+                    },
+                },
+            },
+        };
+    }
+    return manifest;
+}
 
 export async function getManifest() {
-    // update this file to update this manifest.json
-    // can also be conditional based on your need
     return {
-        manifest_version: 2,
+        manifest_version: 3,
         ...info,
         ...browserSpecific(),
         content_scripts: [
@@ -65,44 +114,15 @@ export async function getManifest() {
                 all_frames: true,
             },
         ],
-        background: {
-            scripts: ['dist/background/background.js'],
-        },
-        web_accessible_resources: ['templates.html', 'dist/styles.css', 'images/flags/*.svg'],
         icons: {
             48: 'images/icon_light-48.png',
             64: 'images/icon_light-64.png',
             96: 'images/icon_light-96.png',
             128: 'images/icon_light-128.png',
         },
-        permissions: [
-            'storage',
-            'contextMenus',
-            'https://www.multitran.com/*',
-            'https://*.wiktionary.org/api/*',
-            'https://*.wikimedia.org/*',
-        ],
-        browser_action: {
-            default_title: '__MSG_extension_title__',
-            default_icon: {
-                16: 'images/icon_light-16.png',
-                32: 'images/icon_light-32.png',
-                64: 'images/icon_light-64.png',
-            },
-            ...themeIcons,
-            default_popup: 'popup.html',
-        },
         options_ui: {
             page: 'options.html',
             open_in_tab: true,
-        },
-        commands: {
-            // @ts-ignore
-            _execute_browser_action: {
-                suggested_key: {
-                    default: 'Alt+M',
-                },
-            },
         },
     };
 }
